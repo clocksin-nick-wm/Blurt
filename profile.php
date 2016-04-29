3de58d661de68323874a0180d5e2821f22aba5da
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-04-29 14:48:36 -0700

    added user name to show on title of page, and echoes information that user inputs

--- profile.php
+++ profile.php
@@ -1,10 +1,22 @@
 <?php
-
+include_once('navbar.php');
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+    <title><?php echo $_SESSION['username']; ?>'s Profile</title>
+    <!-- Whatever we decide to chose to have show on the page will work out on the tab putting the information wanted on this tab -->
+</head>
+<body>
+<div id="infoBox_Profile">
+    <?php echo $_SESSION['username']; ?>
+    <?php echo $_SESSION['first_name']; ?>
+    <?php echo $_SESSION['last_name']; ?>
+    <?php echo $_SESSION['email']; ?>
+</div>
 <script src="scripts.js"></script>
 <div id="description">
-    <button onclick="prompt1()">Add a description for yourself</button><div id="bio1" style="margin-top: 5px; word-wrap: break-word"></div>
+    <button onclick="prompt1()">Add a description for this profile</button><div id="bio1" style="margin-top: 5px; word-wrap: break-word"></div>
 </div>
+</body>
 </html>
