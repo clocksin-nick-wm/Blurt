883065943a05d794321f4d88937695376a7deb99
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-05-03 13:47:19 -0700

    messing around with some php for the edit profile page not sure if it works just yet

--- editprofile.php
+++ editprofile.php
@@ -1,4 +1,80 @@
-<?php ?>
+<?php
+$dbh = new PDO('mysql:host=localhost;dbname=mismatchdb', 'root', 'root');
+
+if (isset($_POST['submit'])) {
+    // Grab the profile data from the POST
+    $first_name = trim($_POST['firstname']);
+    $last_name = trim($_POST['lastname']);
+    $email = trim($_POST['email']);
+    $old_picture = trim($_POST['old_picture']);
+    $new_picture = trim($_FILES['new_picture']['name']);
+    $new_picture_type = $_FILES['new_picture']['type'];
+    $new_picture_size = $_FILES['new_picture']['size'];
+    list($new_picture_width, $new_picture_height) = getimagesize($_FILES['new_picture']['tmp_name']);
+    $error = false;
+
+    // Validate and move the uploaded picture file, if necessary
+    if (!empty($new_picture)) {
+        if ((($new_picture_type == 'image/gif') || ($new_picture_type == 'image/jpeg') || ($new_picture_type == 'image/pjpeg') ||
+                ($new_picture_type == 'image/png')) && ($new_picture_size > 0) && ($new_picture_size <= MM_MAXFILESIZE) &&
+            ($new_picture_width <= MM_MAXIMGWIDTH) && ($new_picture_height <= MM_MAXIMGHEIGHT)) {
+            if ($_FILES['file']['error'] == 0) {
+                // Move the file to the target upload folder
+                $target = MM_UPLOADPATH . basename($new_picture);
+                if (move_uploaded_file($_FILES['new_picture']['tmp_name'], $target)) {
+                    // The new picture file move was successful, now make sure any old picture is deleted
+                    if (!empty($old_picture) && ($old_picture != $new_picture)) {
+                        @unlink(MM_UPLOADPATH . $old_picture);
+                    }
+                }
+                else {
+                    // The new picture file move failed, so delete the temporary file and set the error flag
+                    @unlink($_FILES['new_picture']['tmp_name']);
+                    $error = true;
+                    echo '<p class="error">Sorry, there was a problem uploading your picture.</p>';
+                }
+            }
+        }
+        else {
+            // The new picture file is not valid, so delete the temporary file and set the error flag
+            @unlink($_FILES['new_picture']['tmp_name']);
+            $error = true;
+            echo '<p class="error">Your picture must be a GIF, JPEG, or PNG image file no greater than ' . (MM_MAXFILESIZE / 1024) .
+                ' KB and ' . MM_MAXIMGWIDTH . 'x' . MM_MAXIMGHEIGHT . ' pixels in size.</p>';
+        }
+    }
+
+    // Update the profile data in the database
+    if (!$error) {
+        if (!empty($first_name) && !empty($last_name) && !empty($gender) && !empty($birthdate) && !empty($city) && !empty($state)) {
+            // Only set the picture column if there is a new picture
+            if (!empty($new_picture)) {
+                $query = "UPDATE mismatch_user SET first_name = :first_name, last_name = :last_name, email = :email, " .
+                    "picture = :new_picture WHERE user_id = '" . $_SESSION['user_id'] . "'";
+            }
+            else {
+                $query = "UPDATE mismatch_user SET first_name = :first_name, last_name = :last_name, email = :email, " .
+                    "WHERE user_id = '" . $_SESSION['user_id'] . "'";
+            }
+            $stmt = $dbh ->prepare($query);
+            $stmt->execute(array(
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email,
+                'new_picture' => $new_picture,
+                'user_id' => $_SESSION['user_id']));
+
+            // Confirm success with the user
+            echo '<p>Your profile has been successfully updated. Would you like to <a href="viewprofile.php">view your profile</a>?</p>';
+
+            exit();
+        }
+        else {
+            echo '<p class="error">You must enter all of the profile data (the picture is optional).</p>';
+        }
+    }
+}
+?>
 <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo MM_MAXFILESIZE; ?>"/>
     <fieldset>
