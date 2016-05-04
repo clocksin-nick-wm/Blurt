f054e64907da5d91a77875e17ec01d969c20fb62
Gavin Mason <session1@START-Coding-MBP-15.local>
2016-05-04 15:13:56 -0700

    Got log in to work

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+require_once('start_session.php');
 include_once('authenticate.php');
-session_start();
+
 
 // If the session vars aren't set, try to set them with a cookie
 if (!isset($_SESSION['user_id'])) {
