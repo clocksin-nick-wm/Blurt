6b2f1eee7be682b95b41a13053301fc251f3bd2c
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2016-05-05 13:58:47 -0700

    no message

--- login.php
+++ login.php
@@ -1,66 +1,7 @@
 <?php
-/*
-
-$pagetitle = 'Login';
-session_start();
-
-if(!isset($_SESSION['user_id'])) {
-    if (isset($_POST['submit'])) {
-        $username = trim($_POST['username']);
-        $password = trim($_POST['password']);
-        if(!empty($username) && !empty($password)) {
-            $dbh = new PDO('mysql:host=localhost;dbname=blurtdb', 'root', 'root');
-            $query = "SELECT username, password FROM users WHERE username = :username AND  password = SHA(:password)";
-            $stmt = $dbh -> prepare($query);
-            $stmt -> execute(array(
-               ':username' => $username,
-                ':password' => $password
-            ));
-            $result = $stmt -> fetchAll();
-            if(count($result) == 1 ){
-                $row = $result[0];
-                $_SESSION['user_id'] = $row['user_id'];
-                $_SESSION['username'] = $row['username'];
-                setcookie('user_id', $row['user_id'], time() + (60 * 60 * 24 * 30));    // expires in 30 days
-                setcookie('username', $row['username'], time() + (60 * 60 * 24 * 30));  // expires in 30 days
-                $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
-                header('Location: ' . $home_url);
-            }
-            else {
-                // The username/password are incorrect so set an error message
-                $error_msg = 'Sorry, you must enter a valid username and password to log in.';
-            }
-        }
-        else {
-            // The username/password weren't entered so set an error message
-            $error_msg = 'Sorry, you must enter your username and password to log in.';
-        }
-        }
-}
-?>
-<!DOCTYPE html>
-    <html>
-<head>
-    <title>Blurt <?php echo  $pagetitle ?> </title>
-</head>
-<body>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <fieldset>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username">
-        <label for="password">Password:</label>
-        <input id="password" name="password" type="password">
-        <button type="submit" name="submit" id="submit">Submit</button>
-    </fieldset>
-</form>
-</body>
-</html>
-*/
-
   require_once('start_session.php');
 
-  // Start the session
-  session_start();
+
 
   // Clear the error message
   $error_msg = "";
