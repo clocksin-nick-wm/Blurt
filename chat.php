d87746f5c993b9ff331ab7f488769970c29c5ebc
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-05-09 13:49:55 -0700

    Added a chat.php page

---
+++ chat.php
@@ -0,0 +1,128 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="chat.css">
+    <meta charset="UTF-8">
+    <title>Chat</title>
+</head>
+<body>
+<div class="menu">
+    <div class="back"><i class="fa fa-chevron-left"></i> <img src="http://i.imgur.com/ONiOKeA.png" draggable="false"/></div>
+    <div class="name"><i>@draculad</i></div>
+    <div class="last">9:22 P.M.</div>
+</div>
+<ol class="chat">
+    <li class="other">
+        <div class="avatar"><img src="http://i.imgur.com/ONiOKeA.png" draggable="false"/></div>
+        <div class="msg">
+            <p>dude.</p>
+            <p>im boutta eat the heck outta ur pizza <emoji class="pizza"/></p>
+            <p>btw. do we have a test tomorrow?</p>
+            <time>8:17 P.M. </time>
+        </div>
+    </li>
+    <li class="self">
+        <div class="avatar"><img src="http://cdn.shopify.com/s/files/1/0761/2225/t/8/assets/profileIconCustom.png?7584684197308184343" draggable="false"/></div>
+        <div class="msg">
+            <p>what! noo!!</p>
+            <p>and yeah, spanish test, dude. senor hunt gave us a review <emoji class="books"/></p>
+            <p>dont eat my pizza.</p>
+            <time>8:18 P.M.</time>
+        </div>
+    </li>
+    <li class="other">
+        <div class="avatar"><img src="http://i.imgur.com/ONiOKeA.png" draggable="false"/></div>
+        <div class="msg">
+            <p>ah no!!! im gonna fail lol  <emoji class="suffocated"/></p>
+            <time>8:18 P.M.</time>
+        </div>
+    </li>
+    <li class="self">
+        <div class="avatar"><img src="http://cdn.shopify.com/s/files/1/0761/2225/t/8/assets/profileIconCustom.png?7584684197308184343" draggable="false"/></div>
+        <div class="msg">
+            <p>do u want the review pages?</p>
+            <p>only send em if u swear u wont eat my pizza tho</p>
+            <time>8:19 P.M.</time>
+        </div>
+    </li>
+    <li class="other">
+        <div class="avatar"><img src="http://i.imgur.com/ONiOKeA.png" draggable="false"/></div>
+        <div class="msg">
+            <p><emoji class="scream"/></p>
+            <p>dude i wont! please send them!! <emoji class="please"/></p>
+            <time>8:19 P.M.</time>
+        </div>
+    </li>
+    <li class="self">
+        <div class="avatar"><img src="http://cdn.shopify.com/s/files/1/0761/2225/t/8/assets/profileIconCustom.png?7584684197308184343" draggable="false"/></div>
+        <div class="msg">
+            <img src="http://i.imgur.com/QAROObc.jpg" draggable="false"/>
+            <time>8:20 P.M.</time>
+        </div>
+    </li>
+    <li class="other">
+        <div class="avatar"><img src="http://i.imgur.com/ONiOKeA.png" draggable="false"/></div>
+        <div class="msg">
+            <p>gracias! <emoji class="hearth_blue"/></p>
+            <p>i owe u my life</p>
+            <time>8:20 P.M.</time>
+        </div>
+    </li>
+    <div class="day">Today</div>
+    <li class="self">
+        <div class="avatar"><img src="http://cdn.shopify.com/s/files/1/0761/2225/t/8/assets/profileIconCustom.png?7584684197308184343" draggable="false"/></div>
+        <div class="msg">
+            <p>dude! have u seen the update for league?</p>
+            <time>9:03 P.M.</time>
+        </div>
+    </li>
+    <li class="other">
+        <div class="avatar"><img src="http://i.imgur.com/ONiOKeA.png" draggable="false"/></div>
+        <div class="msg">
+            <p>u bet ive seen it</p>
+            <p>new champs name is Taliyah</p>
+            <time>9:07 P.M.</time>
+        </div>
+    </li>
+    <li class="self">
+        <div class="avatar"><img src="http://cdn.shopify.com/s/files/1/0761/2225/t/8/assets/profileIconCustom.png?7584684197308184343" draggable="false"/></div>
+        <div class="msg">
+            <p>yeah!!! she looks so cool tbh <emoji class="cryalot"/></p>
+            <time>9:08 P.M.</time>
+        </div>
+    </li>
+    <li class="other">
+        <div class="avatar"><img src="http://i.imgur.com/ONiOKeA.png" draggable="false"/></div>
+        <div class="msg">
+            <p>crazy brows tho lmao<emoji class="lmao"/></p>
+            <time>9:08 P.M.</time>
+        </div>
+    </li>
+    <li class="self">
+        <div class="avatar"><img src="http://cdn.shopify.com/s/files/1/0761/2225/t/8/assets/profileIconCustom.png?7584684197308184343" draggable="false"/></div>
+        <div class="msg">
+            <p>dude im so excited </p>
+            <time>9:09 P.M.</time>
+        </div>
+    </li>
+    <li class="other">
+        <div class="avatar"><img src="http://i.imgur.com/ONiOKeA.png" draggable="false"/></div>
+        <div class="msg">
+            <p>me too. u wanna play sometime?</p>
+            <time>9:10 P.M.</time>
+        </div>
+    </li>
+    <li class="self">
+        <div class="avatar"><img src="http://cdn.shopify.com/s/files/1/0761/2225/t/8/assets/profileIconCustom.png?7584684197308184343" draggable="false"/></div>
+        <div class="msg">
+            <p>dude. of course. we should invite austin too</p>
+            <time>9:11 P.M.</time>
+        </div>
+    </li>
+</ol>
+<input class="textarea" type="text" placeholder="Type here!"/><div class="emojis"></div>
+</body>
+</html>
