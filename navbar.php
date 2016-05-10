0415eb8b80d4e0942399dd9f08afe9ef997b7427
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2016-05-10 14:53:52 -0700

    Glyph icons added to navbar

--- navbar.php
+++ navbar.php
@@ -6,11 +6,11 @@
         <ul class="nav navbar-nav">
             <li><a href="index.php">Home</a></li>
             <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
-            <li><a href="editprofile.php">Edit Profile</a></li>
+            <li><a href="editprofile.php"><span class="glyphicon glyphicon-pencil"></span> Edit Profile</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li ><a href="newpost.php"><span class="glyphicon glyphicon-pencil"></span> Create Post</a></li>
-            <li><a href="logout.php">Logout (<?php echo $_SESSION['username'] ?>)</a> </li>
+            <li ><a href="newpost.php"><span class="glyphicon glyphicon-edit"></span> Create Post</a></li>
+            <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout (<?php echo $_SESSION['username'] ?>)</a> </li>
         </ul>
     </div>
 </nav>
