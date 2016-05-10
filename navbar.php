9a7fa76458c43704324f7ce35fe4e797dc88d1c7
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-05-10 13:43:15 -0700

    Added infinity to the navbar page

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <?php
 echo '<a href="index.php">Posts</a>&infin;';
 echo '<a href="view.php">My Posts</a>&infin;';
-echo '<a href="editprofile.php">Edit Profile</a>';
+echo '<a href="editprofile.php">Edit Profile</a>&infin;';
 echo '<a href="logout.php">Logout</a>';
 ?>
