1a4f7592a00ff1e0c8fa6596c9f787c7eb72b030
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-05-10 13:41:56 -0700

    changed what file is required in the edit profile, and took away double post of the user information

--- profile.php
+++ profile.php
@@ -55,17 +55,5 @@ else {
     <title><?php echo $_SESSION['username']; ?>'s Profile</title>
     <!-- Whatever we decide to chose to have show on the page will work out on the tab putting the information wanted on this tab -->
 </head>
-<body>
-<?php echo $_SESSION['profile_pic']; ?>
-<div id="infoBox_Profile">
-    <form>
-
-        <label>Username: <?php echo $_SESSION['username'] ?></label> <br>
-        <label>First Name: <? echo $_SESSION['first_name']?></label> <br>
-        <label>Last Name: <?php echo $_SESSION['last_name'] ?></label> <br>
-        <label>Email: <?php echo $_SESSION['email'] ?></label> <br>
-    <!-- User information given in account making -->
-        <label>Description: <?php echo $_SESSION['description'] ?></label> <br>
-    </form>
-</div>
+</html>
 
