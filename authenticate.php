18227028b0769b2e22e5b47e3edbf3bae5839aef
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2016-05-10 13:40:22 -0700

    Working post page with newport.php. Edited authentication page to remove the return for login/signup.php to profilelogin.php. Also deleted signup.php

--- authenticate.php
+++ authenticate.php
@@ -1,6 +1,6 @@
 <?php
 if(!isset($_SESSION['user_id'])){
-echo '<p>You must be <a href="login.php"> logged in</a> to view this page or <a href="signup.php"> create an account</a></p>';
+echo '<p>You must be <a href="profilelogin.php"> logged in</a> to view this page or <a href="profilelogin.php"> create an account</a></p>';
 exit();
 }
 ?>
