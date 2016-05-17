15a8896a3f59875375f9da04ac65893fb084dbe1
Start Coding Student 1 <session1@start-coding-mbp-c02pp71ug8wn.local>
2016-05-17 14:12:51 -0700

    no message

--- view.php
+++ view.php
@@ -30,29 +30,6 @@ else {
 //$_SESSION['id'] = 1;
 //echo $_SESSION['id'] . "<- session";
 foreach ($data as $row) {
-    // The user row was found so display the user data
-    //$row = $data[0];
-    echo '<table>';
-    if (!empty($row['username'])) {
-        echo '<tr><td >Username: </td><td>' . $row['username'] . '</td></tr>';
-    }
-    if (!empty($row['first_name'])) {
-        echo '<tr><td>First name: </td><td>' . $row['first_name'] . '</td></tr>';
-    }
-
-    if (!empty($row['description'])) {
-        echo '<tr><td>Description:</td><td>' . $row['description'] . '</td></tr>';
-    }
-    if (!empty($row['picture'])) {
-        echo '<tr><td class="label">Picture:</td><td><img src="' . MM_UPLOADPATH . $row['picture'] .
-            '" alt="Profile Picture" /></td></tr>';
-    }
-    echo '</table>';
-    if (!isset($_GET['user_id']) || ($_SESSION['user_id'] == $_GET['user_id'])) {
-        echo '<p>Would you like to <a href="editprofile.php">edit your profile</a>?</p>';
-    }
-} // End of check for a single row of user results
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -61,7 +38,22 @@ foreach ($data as $row) {
     <link rel="stylesheet" href="index.css" type="text/css">
 </head>
 <body>
+<div class="user-profile">
+    <img class="avatar"
+         src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTF_erFD1SeUnxEpvFjzBCCDxLvf-wlh9ZuPMqi02qGnyyBtPWdE-3KoH3s"
+         alt="Ash"/>
+    <div class="username"><?php echo $row['username']; ?></div>
+    <div class="bio">
+
+    </div>
+    <div class="description">
+        <?php
+        echo $row['description'];
+        ?>
+    </div>
+</div>
 <?php
+}
 $dbh = new PDO ('mysql:host=localhost;dbname=blurtdb;', 'root', 'root');
 $query = "SELECT * FROM posts WHERE user_id = '" . $_GET['user_id'] .  "' ORDER BY post_time DESC";
 $stmt = $dbh ->prepare($query);
